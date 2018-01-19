f1b1e3b362a07b393f4cd4cf88f1546931703889
junaidnz97 <junaidnz97@gmail.com>
2018-01-19 11:26:02 +0530

    Update ARMYPARADE.cpp

--- HACKEREARTH/OCTOBER CIRCUITS -17/ARMYPARADE.cpp
+++ HACKEREARTH/OCTOBER CIRCUITS -17/ARMYPARADE.cpp
@@ -3,52 +3,42 @@ using namespace std;
 
 #define mod 2000000033
 
-
-// A modular inverse based solution to
-// compute nCr % p
 #include<bits/stdc++.h>
 using namespace std;
  
-/* Iterative Function to calculate (x^y)%p
-  in O(log y) */
+
 long long int power(long long int x, long long int y, long long int p)
 {
-    long long int res = 1;      // Initialize result
+    long long int res = 1;      
  
-    x = x % p;  // Update x if it is more than or
-                // equal to p
+    x = x % p;  
+                
  
     while (y > 0)
     {
-        // If y is odd, multiply x with result
+        
         if (y & 1)
             res = (res*x) % p;
  
-        // y must be even now
-        y = y>>1; // y = y/2
+       
+        y = y>>1; 
         x = (x*x) % p;
     }
     return res;
 }
  
-// Returns n^(-1) mod p
+
 long long int modInverse(long long int n, long long int p)
 {
     return power(n, p-2, p);
 }
  
-// Returns nCr % p using Fermat's little
-// theorem.
+
+
 long long int nCrModPFermat(long long int n, long long int r, long long int p,vector<long long int> &fac)
 {
-   // Base case
    if (r==0)
-      return 1;
- 
-    // Fill factorial array so that we
-    // can find all factorial of r, n
-    // and n-r
-   
+      return 1;  
  
     return (fac[n]* modInverse(fac[r], p) % p *
             modInverse(fac[n-r], p) % p) % p;
